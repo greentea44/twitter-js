cc13bc2270eb62e26bb26dba3235a59974b68dad
Mary Irwin <maldenirwin@gmail.com>
2017-09-19 11:14:28 -0400

    new changes

--- app.js
+++ app.js
@@ -7,26 +7,28 @@ app.use('/', function(req, res, next) {
   next();
 })
 
-app.get('/', function(req, res) {
-  res.send("Welcome")
-})
-
-app.listen(3000, function() {
-  console.log('server listening');
-})
-
-title: 'An Example',
+var locals = {title: 'An Example',
 people: [
     { name: 'Gandalf'},
     { name: 'Frodo' },
     { name: 'Hermione'}
 ]
 };
+
+app.get('/', function(req, res) {
+  res.render('index', locals);
+})
+
+app.listen(3000, function() {
+  console.log('server listening');
+})
+
+
 nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-console.log(output);
-});
+// nunjucks.render('index.html', locals, function (err, output) {
+// console.log(output);
+// });
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+// point nunjucks to the proper directory for templates
