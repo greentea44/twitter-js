aa481ae14f312c524c3003f9bc815b8452832b9d
Mary Irwin <maldenirwin@gmail.com>
2017-09-19 10:43:07 -0400

    changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require( 'express' );
+const nunjucks = require('nunjucks');
 const app = express();
 
 app.use('/', function(req, res, next) {
@@ -13,3 +14,19 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
   console.log('server listening');
 })
+
+title: 'An Example',
+people: [
+    { name: 'Gandalf'},
+    { name: 'Frodo' },
+    { name: 'Hermione'}
+]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+console.log(output);
+});
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
